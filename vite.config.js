cb0808ae83428117f62f1b81e44cb5dba271131e
Dang Van Y <y.dangvan@gmail.com>
2025-09-17 14:31:14 +0700

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-component-gallery/',
+  base: '/ARtech/',
   build: {
     outDir: 'dist'
   }
